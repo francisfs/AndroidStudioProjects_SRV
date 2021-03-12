e3d2a4b8f7a51366d2c1b974c61153629bf9b798
FRANCISLEY <55454628+francisfs@users.noreply.github.com>
2021-03-12 18:28:01 -0300

    Update MainActivity.java

--- ConsultaCep/app/src/main/java/com/example/consultacep/MainActivity.java
+++ ConsultaCep/app/src/main/java/com/example/consultacep/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.consultacep;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,22 +8,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import java.util.concurrent.ExecutionException;
-
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button btnConsultarProg = (Button) findViewById(R.id.btnConsultar);
         final EditText cep = findViewById(R.id.edtCEP);
         final TextView resultado = findViewById(R.id.txtResultado);
 
         btnConsultarProg.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                try {
+               try {
                     CEP retorno = new CEP();
                     ConsumirJSON cj = new ConsumirJSON(cep.getText().toString());
                     retorno = cj.execute().get();
