562521da3fbca029928f1274a0557b8255f6f5ef
FRANCISLEY <55454628+francisfs@users.noreply.github.com>
2021-03-12 17:51:46 -0300

    Initial commit

---
+++ ConsultaCep/app/src/main/java/com/example/consultacep/CEP.java
@@ -0,0 +1,51 @@
+package com.example.consultacep;
+
+public class CEP {
+    private String logradouro;
+    private String bairro;
+    private String localidade;
+    private String uf;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouroç(String logradouroç) {
+        this.logradouro = logradouroç;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    @Override
+    public String toString(){
+        return
+        "Logradouro: " + getLogradouro()
+        + "\nBairro: " + getBairro()
+        + "\nCidade: " + getLocalidade()
+        + "\nEstado: " + getUf();
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AndroidStudioProjects_SRV
+ 
